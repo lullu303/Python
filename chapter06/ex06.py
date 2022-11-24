cce5cc18c8016e2d33c63f3d54113ba3a813ed3b
ji hye park <109124273+lullu303@users.noreply.github.com>
2022-11-24 17:37:17 +0900

    Add files via upload

---
+++ chapter06/ex06.py
@@ -0,0 +1,8 @@
+import random as rd
+rd.random()     #0이상 1 미만인 실수를 반환함
+
+rd.random()     #매번 다른 실수를 반환함
+rd.randrange(1,7)   #1이상 7미만의 정수를 반환함
+
+rd.randrange(0, 10, 2)  #1이상 10 미만 정수 중 2의 배수를 반환함
+rd.randint(1,10) #1 이상 10 이하의 (1,10이 포함) 임의의 정수를 반환
