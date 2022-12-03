38b378e25bdb1767eaf21897c091f7cb89b5317e
ji hye park <109124273+lullu303@users.noreply.github.com>
2022-12-04 00:42:18 +0900

    Add files via upload

---
+++ 2022-12-04/lab2-5.py
@@ -0,0 +1,26 @@
+globall = 300
+print(globall)
+
+width = 20
+height = 40
+area = ( width * height)
+print('사각형의 면적 : ', area)
+
+iixxjjkk = 20
+print('나의 나이는', iixxjjkk, '세 입니다')
+
+v1 = 1
+v2 = 2
+thistisaverylongvariablename = v1 + v2
+print(thistisaverylongvariablename)
+
+redius = 20
+depth = 10
+final_length = 0.5 * redius * depth
+print('삼각형의 면적은', final_length)
+
+first_year = 30
+second_year = 30
+third_year = 16
+all_years = first_year + second_year + third_year
+print(all_years)
