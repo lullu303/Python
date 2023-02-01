d86e90d3a3285b8b80aa3651eb049968a763c4b1
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-01 17:16:28 +0900

    Add files via upload

---
+++ 2023-02-01/pre_argument.py
@@ -0,0 +1,14 @@
+def a_rectangle_area():
+    print(5 * 7)    #매개변수x, 반환값x
+
+def b_rectangle_area(x, y):
+    print(x * y)    #매개변수o, 반환값x
+
+def c_rectangle_grea():
+    return( 5 * 7)  #매개변수x, 반환값o
+
+def d_rectangle_area(x,y):
+    return(x * y)   #매개변수o, 반환값o
+
+result1 = d_rectangle_area(3,3)
+print(result1)
