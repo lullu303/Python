0113787e6c941a58f24203cd2560028109521002
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-01 17:16:12 +0900

    Add files via upload

--- 2023-02-01/guess_game.py
+++ 2023-02-01/guess_game.py
@@ -1,19 +1,24 @@
 print("Guess game 입니다~~^^")
 
 import random
-for i in range(1):
-    game = random.randint(1,100) #랜덤하게 숫자를 추출
+game = random.randint(1,100) #랜덤하게 숫자를 추출
 
-guest_input = input("숫자를 입력해주세요: ")
+count = 0
 
-guest_input_int = int(guest_input) #int로 형변환
-if i == guest_input_int : 
-     print(f"정답입니다 {guest_input_int, game}")
+while True: 
+    count +=1
 
-elif i > guest_input_int :
-    print(f"숫자가 너무 큽니다 {guest_input_int, game}")
-    
-elif i < guest_input_int :
-    print(f"숫자가 너무 작습니다 {guest_input_int, game}")
+    guest_input = input("숫자를 입력해주세요: ")
 
+    guest_input_int = int(guest_input) #int로 형변환
+
+    if game == guest_input_int : 
+        print(f"정답입니다! 입력한 숫자는 {guest_input_int}, {count}만에 맞추셨습니다!")
+
+    elif game < guest_input_int :
+         print(f"숫자가 너무 큽니다! 입력한 숫자는 {guest_input_int}, 임의의 숫자는 {game} 입니다")
+        
+    elif game > guest_input_int :
+        print(f"숫자가 너무 작습니다!입력한 숫자는 {guest_input_int}, 임의의 숫자는 {game} 입니다.") 
+            
    
