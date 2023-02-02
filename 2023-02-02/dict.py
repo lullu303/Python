fe572f182a9d29ad0ab03f9ec7bf97dba8fc8ab0
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-02 14:36:50 +0900

    Add files via upload

---
+++ 2023-02-02/dict.py
@@ -0,0 +1,12 @@
+# 딕셔너리 - 자바: 해시 맵
+    # 파이썬에서 많이 사용하는 자료구조
+
+student_info = {20140012: '남도산', 20140029:'서달미', 20150235:'사혜준', 20150098:'안정하'}
+print(student_info[20140012])
+
+type(student_info) #dict
+
+student_info[20150125] = '김진우'   # 재할당
+print(student_info[20150125])
+
+print(student_info)
