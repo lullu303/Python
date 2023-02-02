8c178ab4eb185f872945c1eee78d65685b709858
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-02 14:42:40 +0900

    Add files via upload

---
+++ 2023-02-02/dict_for.py
@@ -0,0 +1,8 @@
+# 딕셔너리의 함수
+    # for문으로 딕셔너리 아이템을 출력
+
+student_info = {20140012: '남도산', 20140029:'서달미', 20150235:'사혜준', 20150098:'안정하'}
+
+for k, v in student_info.items() :
+    print("key: ", k)
+    print("value: ", v)
