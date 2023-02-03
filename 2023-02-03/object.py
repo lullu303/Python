e2ce226626e52b73b877c4909003aa4ab4542ca4
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-03 15:30:49 +0900

    Add files via upload

--- 2023-02-03/object.py
+++ 2023-02-03/object.py
@@ -5,7 +5,7 @@ class SoccerPlayer(object) :
         self.back_number = back_number
     # self 변수는 클래스에서 생성된 인스턴스에 접근하는 예약어 - 생성된 인스턴스를 지정하는 변수
     # self를 매개변수에 반드시 넣어야 함. self가 있어야 인스턴스가 사용할 수 있는 함수로 선언됨.
-    
+
     def change_back_number(self, new_number) :
         print("선수의 등 번호를 변경한다: : From %d to %d " % (self.back_number, new_number))
         self.back_number = new_number
