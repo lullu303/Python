bd12f6d037fad07f7389c06f40c144734a8ae524
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-07 14:20:17 +0900

    Update assert_test.py

--- 2023-02-07/assert_test.py
+++ 2023-02-07/assert_test.py
@@ -3,5 +3,6 @@
 def get_binary_number(decimal_number):
     assert isinstance(decimal_number, int)
     return bin(decimal_number)
-print(get_binary_number(10))
-print(get_binary_number("10"))
+
+print(get_binary_number(10)) # 0b1010
+print(get_binary_number("10")) # AssertionError
